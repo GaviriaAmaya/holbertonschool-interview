f9055b2f7e096dc5c15efd6800ad7767eea2c364
GaviriaAmaya <729@holbertonschool.com>
2020-08-04 23:01:31 -0500

    [Add*] Heap insert solved

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,85 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: binary tree
+ * Return: the size of @tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) +
+		binary_tree_size(tree->right));
+}
+
+/**
+ * starting - Finds the starte
+ * @node: Node starter
+ * Return: Pointer to root
+ */
+heap_t *starting(heap_t *node)
+{
+	int temp;
+
+	if (!node || !node->parent)
+		return (node);
+
+	while (node->parent)
+	{
+		if (node->n < node->parent->n)
+			return (node);
+		temp = node->n;
+		node->n = node->parent->n;
+		node->parent->n = temp;
+		node = node->parent;
+	}
+	return (node);
+}
+
+/**
+ * heap_insert - inserts a value into a Max Binary Heap
+ *
+ * @root: Pointer to pointer to root node of Heap
+ * @value: to be inserted
+ *
+ * Return: pointer to inserted node or NULL on failure
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+	int bitIndex = 0;
+	int mask;
+	ssize_t n = binary_tree_size(*root) + 1;
+	heap_t *new_node = NULL;
+	heap_t *node = *root;
+
+	new_node = binary_tree_node(node, value);
+	if (!new_node)
+		return (NULL);
+	if (!*root)
+		return (*root = new_node);
+
+	while (1)
+	{
+		if (1 << (bitIndex + 1) > n)
+			break;
+		bitIndex++;
+	}
+
+	bitIndex--;
+
+	for (; bitIndex > 0; bitIndex--)
+	{
+		mask = (1 << bitIndex);
+		if (n & mask)
+			node = node->right;
+		else
+			node = node->left;
+	}
+	if (n & 1)
+		node->right = new_node;
+	else
+		node->left = new_node;
+	new_node->parent = node;
+	return (starting(new_node));
+}
