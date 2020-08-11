63d10deb33f1e7c31faf4a1661fe05fb9acab736
GaviriaAmaya <729@holbertonschool.com>
2020-08-11 17:39:46 -0500

    [Fix] Pycodestyle fixed. Delete an unnecessary jump on an iteration

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,6 +7,7 @@ Given a number n, write a method that calculates the fewest number of
 operations needed to result in exactly n H characters in the file.
 """
 
+
 def minOperations(n):
     """ Calculates the fewest number of operations:
         minOperations(12) = 7
@@ -14,11 +15,8 @@ def minOperations(n):
     if n <= 1:
         return 0
     else:
-        # The minimum operations of any n, initially is
-        # Copy All and Paste
         ops = 2
         for i in range(2, n + 1):
             if n % i == 0:
                 ops += 1
-                i += 1
         return ops
