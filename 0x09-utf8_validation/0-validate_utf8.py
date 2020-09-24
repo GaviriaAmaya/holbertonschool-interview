a93a31a40ed7d9b2338638aebaf4378de748db5c
GaviriaAmaya <729@holbertonschool.com>
2020-09-23 23:52:57 -0500

    [Add] Naive approaching for the UTF-8 problem

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,28 +3,28 @@
 
 
 def validUTF8(data):
-	" Method that receives an array and returns boolean "
-	current_bytes = 0
+    " Method that receives an array and returns boolean "
+    current_bytes = 0
 
-	for num in data:
-		binary_rep = format(num, '#010b')[-8:]
+    for num in data:
+        binary_rep = format(num, '#010b')[-8:]
 
-	if current_bytes == 0:
-		for bit in binary_rep:
-			if bit == '0':
-				break
-			current_bytes += 1
-		
-		if current_bytes == 0:
-			continue
+    if current_bytes == 0:
+        for bit in binary_rep:
+            if bit == '0':
+                break
+            current_bytes += 1
 
-		if current_bytes == 1 or current_bytes > 4:
-			return False
+            if current_bytes == 0:
+                continue
 
-	else:
-		if not (binary_rep[0] == '1' and binary_rep == '0'):
-			return False
-	
-	current_bytes -= 1
+        if current_bytes == 1 or current_bytes > 4:
+            return False
 
-	return current_bytes == 0
+    else:
+        if not (binary_rep[0] == '1' and binary_rep == '0'):
+            return False
+
+    current_bytes -= 1
+
+    return current_bytes == 0
