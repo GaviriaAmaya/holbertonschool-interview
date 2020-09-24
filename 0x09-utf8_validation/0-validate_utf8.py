2fd922207c40c5b1d2f7650daef1ab7cb98a017b
GaviriaAmaya <729@holbertonschool.com>
2020-09-23 23:47:33 -0500

    [Add] Naive approaching for the UTF-8 problem

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def validUTF8(data):
+	current_bytes = 0
+
+	for num in data:
+		binary_rep = format(num, '#010b')[-8:]
+
+	if current_bytes == 0:
+		for bit in binary_rep:
+			if bit == '0':
+				break
+			current_bytes += 1
+		
+		if current_bytes == 0:
+			continue
+
+		if current_bytes == 1 or current_bytes > 4:
+			return False
+
+	else:
+		if not (binary_rep[0] == '1' and binary_rep == '0'):
+			return False
+	
+	current_bytes -= 1
+
+	return current_bytes == 0
